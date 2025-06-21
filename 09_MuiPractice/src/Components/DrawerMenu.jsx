42c13755bb4947ca859933aab9523e6d6e452f3a
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-06-21 19:31:04 +0530

    Mui

--- 09_MuiPractice/src/Components/DrawerMenu.jsx
+++ 09_MuiPractice/src/Components/DrawerMenu.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Button, CircularProgress, Drawer, List, ListItemButton, ListItemText } from '@mui/material'
+import React, { useState } from 'react'
 
 const DrawerMenu = () => {
+
+    const [open, setOpen] = useState(false);
+    const Menu = ["Home", "About", "Hello", "Hey"]
+
   return (
     <div>
-      
+        {/* <Button variant='contained' onClick={() => setOpen(true)}>Open</Button>
+        <Drawer open={open} onClick={() => setOpen(false)}>
+            <List>
+                {Menu.map(Menus => (
+                    <ListItemButton>
+                        <ListItemText primary={Menus}/>
+                    </ListItemButton>
+                ) )}
+            </List>
+        </Drawer> */}
+        <CircularProgress variant='indeterminate' color='secondary' value={75}>
+            hello
+        </CircularProgress>
+        
     </div>
   )
 }
